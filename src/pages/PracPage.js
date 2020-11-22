c2b284760c08b79b995ddf4c0fe5596ced463ff5
MatHook <mathook00@gmail.com>
2020-11-22 21:03:05 +0300

    Some fixes

--- src/pages/PracPage.js
+++ src/pages/PracPage.js
@@ -1,44 +1,48 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { CodeBlock, monoBlue } from "react-code-blocks";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Container } from "react-bootstrap";
 
 const PracPage = () => {
   const [tasks, setTasks] = useState([]);
 
+  //TODO: make options to choose directory
   useEffect(() => {
     axios
-      .get("https://api.github.com/repos/MatHook/FDT/contents/YndTest")
+      .get("https://api.github.com/repos/MatHook/FDT/contents/Arrays")
       .then(({ data }) => {
         data.forEach((item) => {
-          axios
-            .get(item.download_url)
-            .then((res) => {
-              setTasks((tasks) => [...tasks, res]);
-            })
-            .catch((err) => {
-              console.error(err);
-            });
+          if (item.type === "file")
+            axios
+              .get(item.download_url)
+              .then((res) => {
+                console.log(data);
+                setTasks((tasks) => [...tasks, res]);
+              })
+              .catch((err) => {
+                console.error(err);
+              });
         });
       });
   }, []);
 
   return (
     <Container>
-      <div>Prac Page</div>
-      <Row>
-        {tasks.map((item, key) => (
-          <Col>
+      <h1>Prac Page</h1>
+      {tasks.map((item, key) => (
+        <Row key={key}>
+          <div className="w-100 p-3">
+            <span>{key + 1} Task</span>
             <CodeBlock
               text={item.data}
               language={"javascript"}
               theme={monoBlue}
               showLineNumbers={true}
-              key={tasks.indexOf(key)}
+              codeBlock={true}
             />
-          </Col>
-        ))}
-      </Row>
+          </div>
+        </Row>
+      ))}
     </Container>
   );
 };
