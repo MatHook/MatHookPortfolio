dac0b9526f704798718eec26796a7721af51e12c
MatHook <mathook00@gmail.com>
2020-11-23 15:35:54 +0300

    Edited filtration for blog

--- src/services/insta.js
+++ src/services/insta.js
@@ -9,8 +9,9 @@ export async function fetchInstagramPosts(accountURL) {
   const json = JSON.parse(response.data.match(instagramRegExp)[1]);
   const edges = json.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges.splice(
     0,
-    12
+    11
   );
+  edges.splice(7,2)
   return edges.map(({ node }) => ({
     url: `https://www.instagram.com/p/${node.shortcode}`,
     thubnailUrl: node.thubnail_src,
