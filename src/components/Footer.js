d794bf0a1a0863eef670b075b99608d716b4534b
MatHook <mathook00@gmail.com>
2020-11-26 21:03:52 +0300

    Added HTML5 audion to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,15 @@ export default function Footer() {
     <footer className="mt-5 border-top">
       <Container fluid={true}>
         <Row className="justify-content-between p-3 container mx-auto">
-          <Col className="p-0" md={3} sd={12}>
+          <Col className="p-0 d-inline-flex" md={3} sd={12}>
             Just a portfolio ¬_¬
+            <audio
+              id="music"
+              className="w-10"
+              controls
+              autoplay
+              src="https://drive.google.com/uc?id=1Xxn2kpZQmE-aEEJR4Ud29Tp_3UtQGqko&export=download"
+            ></audio>
           </Col>
           <Col className="p-0 d-flex justify-content-end" md={6}>
             Copyright @ 2020 - Matthew Hook. All rights Reserved
