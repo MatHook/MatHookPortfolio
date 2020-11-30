af5320608720fb846b900df097105ea675358ddc
MatHook <mathook00@gmail.com>
2020-11-30 23:30:41 +0300

    Added new contact links to the contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Hero from "../components/Hero";
 import { Row, Col, Container } from "react-bootstrap";
-import { FaTelegramPlane, FaGithub } from 'react-icons/fa'
+import { FaTelegramPlane, FaGithub, FaLinkedin } from "react-icons/fa";
 import { SiGmail } from 'react-icons/si'
+import { IoDocumentAttachSharp } from "react-icons/io5";
 import Icon24LogoVk from '@vkontakte/icons/dist/24/logo_vk';
 
 function ContactPage(props) {
@@ -10,7 +11,7 @@ function ContactPage(props) {
     <Container fluid={true}>
       <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
       <Container>
-        <Row className="p-0 justify-content-center" sm={2} md={4}>
+        <Row className="p-0 justify-content-center" sm={2} md={6}>
           <Col>
             <FaTelegramPlane className="icon" size={25} />
             <a
@@ -53,6 +54,28 @@ function ContactPage(props) {
               rel="noopener noreferrer"
             >
               GitHub
+            </a>
+          </Col>
+          <Col>
+            <FaLinkedin className="icon" size={25} />
+            <a
+              className="contact"
+              href="https://linkedin.com/in/matthew-kryuchkov-b67991193/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkIn
+            </a>
+          </Col>
+          <Col>
+            <IoDocumentAttachSharp className="icon" size={25} />
+            <a
+              className="contact"
+              href="https://hh.ru/resume/8e175bd2ff06ed421b0039ed1f486954754669"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              HH
             </a>
           </Col>
         </Row>
