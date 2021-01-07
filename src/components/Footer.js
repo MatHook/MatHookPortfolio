4fd0df84630163e5c65a8a728ec30f6a1809b2b4
MatHook <mathook00@gmail.com>
2021-01-07 21:32:30 +0300

    Commit: came back from deep drunk session Removed particles Edited Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,16 +9,9 @@ export default function Footer() {
         <Row className="justify-content-between p-3 container mx-auto">
           <Col className="p-0 d-inline-flex" md={3} sd={12}>
             Just a portfolio ¬_¬
-            <audio
-              id="music"
-              className="w-10"
-              controls
-              autoplay
-              src="https://drive.google.com/uc?id=1Xxn2kpZQmE-aEEJR4Ud29Tp_3UtQGqko&export=download"
-            ></audio>
           </Col>
           <Col className="p-0 d-flex justify-content-end" md={6}>
-            Copyright @ 2020 - Matthew Hook. All rights Reserved
+            Copyright @ 2021 - Matthew Hook. All rights Reserved
           </Col>
         </Row>
       </Container>
